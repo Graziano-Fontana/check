05e52166fad59ae9ea816e889c18a8e4e72262c4
Graziano Fontana <86143141+Graziano-Fontana@users.noreply.github.com>
2021-10-28 17:25:01 +0700

    up

--- xfce4.sh
+++ xfce4.sh
@@ -4,12 +4,10 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp --region us 3389 &>/dev/null &
-echo Downloading File From akuh.net
+nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 echo "===================================="
 echo "Wait 2 Minutes"
 echo "This is only for education"
-echo "Other interesting tutorials visit akuh.net"
 echo "===================================="
 sudo apt-get update > /dev/null 2>&1
 echo "===================================="
@@ -36,7 +34,6 @@ sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "More  Free VPS akuh.net"
 echo "Don't close this tab RDP runs 12 hours"
 echo "Keep support akuh.net thank you"
 echo "===================================="
